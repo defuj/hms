bc9195b8f800afeaede15662cd62745a6a4a7432
Dede Fuji Abdul <defuj.official@gmail.com>
2023-07-27 12:41:50 +0700

    first commit

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'dark': '#1A1313',
+                'yellow-bg': ' #FAF7F4',
+                'primary': {
+                    '50': '#e6f0e6',
+                    '100': '#b0d0b2',
+                    '800': '#003904',
+                },
+            }
+        }
+    }
+}
